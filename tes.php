7fbfa75973457f91b63e70c39916dcdf52230c04
eriknugraha <erik.nugraha10@gmail.com>
2018-01-09 14:23:44 +0700

    test

--- tes.php
+++ tes.php
@@ -8,7 +8,6 @@ class tes extends PHPUnit_Framework_TestCase
                 $query=mysql_query("SELECT * FROM user_man ");
                 $user = mysql_num_rows($query);
                 $content1 = $user;
-            
                 $this->assertEquals(8, $content1);
         }
         function testnama()
@@ -31,6 +30,7 @@ class tes extends PHPUnit_Framework_TestCase
             
                 $this->assertEquals('1.3.PNG', $content3);
         }
+        //teststatus
         function teststatus()
         {
                 include("config/koneksi.php");
@@ -38,7 +38,6 @@ class tes extends PHPUnit_Framework_TestCase
                 $user = mysql_fetch_array($query);
                 $usernya = $user['status'];
                 $content4 = $usernya;
-            
                 $this->assertEquals('non-member', $content4);
         }
 
